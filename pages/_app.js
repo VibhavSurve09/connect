39c586b5f659524b0f66adb9bc226c3d0fd2c4ae
Vibhav0 <vibhav.surve@xaviers.edu.in>
2021-12-19 22:18:01 +0530

    removed Navbar import from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,6 @@
-import Navbar from "../layout/Navbar";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
-  return (
-    <Navbar>
-      <Component {...pageProps} />
-    </Navbar>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
