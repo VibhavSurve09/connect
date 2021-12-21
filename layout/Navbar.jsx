0751a53b33effc7124027925aab59d1919db268d
pandeyutkarsh17 <utkarshpandey138@gmail.com>
2021-12-21 16:25:55 +0530

    navbar color

--- layout/Navbar.jsx
+++ layout/Navbar.jsx
@@ -1,60 +1,46 @@
-//TODO: Use next/link instead of <a>
-//TODO: Use next/Image instead of <img>
-function Navbar() {
+import Link from 'next/link';
+import styles from "./Navbar.module.css";
+export default function Navbar() {
   return (
-    <div>
-      <nav className='flex items-center justify-between flex-wrap bg-red-500 p-6'>
-        <div className='flex items-center flex-shrink-0 text-black mr-6'>
-          <svg
-            className='fill-current h-8 w-8 mr-2'
-            width='54'
-            height='54'
-            viewBox='0 0 54 54'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path d='M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z' />
-          </svg>
-          <span className='font-bold text-xl '>
-            <h1 className='hover:font-black'>
-              <a href='#'>ConnectU📱</a>
+      <nav className='flex items-center justify-between flex-wrap bg-gradient-to-r from-indigo-300 via-indigo-200 to-indigo-50 p-6 relative'>
+      
+          <div className={`text-sm relative ${styles.random}`}>
+
+          <div
+              className='font-bold text-black text-xl items-center flex-shrink-0 inline-block px-5 py-2 font-serif'
+            >
+            <h1>
+              <Link href='#'>📱 ConnectU</Link>
             </h1>
-          </span>
-        </div>
+            </div>
 
-        <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
-          <div className='text-sm lg:flex-grow'>
-            <a
-              href='#responsive-header'
-              className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 text-xl hover:text-black ml-10 mr-4 hover:font-semibold hover:text-2xl font-medium'
+            <div
+              className='inline-block lg:mt-0 px-5 py-2 leading-none text-white text-xl hover:text-black hover:font-semibold hover:text-xl font-bold'
             >
-              Home
-            </a>
-            <a
-              href='#responsive-header'
-              className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 text-xl hover:text-black ml-2 mr-4 hover:font-semibold hover:text-2xl font-medium'
-            >
+              <Link href='#'>
               About
-            </a>
+              </Link>
+            </div>
           </div>
-          <div>
-            <a
-              href='#'
-              className='inline-block text-base px-5 py-2 leading-none border rounded bg-gray-200 text-black border-black hover:border-transparent font-semibold hover:text-blue-400 hover:font-black hover:bg-white mt-4 lg:mt-0'
+          <div className={styles.random1}>
+            <div
+              className='inline-block text-base px-5 py-2 leading-none border rounded bg-gray-200 text-black border-black hover:border-transparent font-semibold hover:text-purple-800 hover:font-black hover:bg-teal-100 mt-4 lg:mt-0'
             >
+              <Link href='#'>
               Log in
-            </a>
+              </Link>
+            </div>
 
-            <a
-              href='#'
-              className='inline-block text-base px-5 py-2 leading-none border rounded bg-gray-200 text-black border-black hover:border-transparent font-semibold hover:text-blue-400 hover:font-black hover:bg-white mt-4 lg:mt-0 ml-4'
+            <div
+              className='inline-block text-base px-5 py-2 leading-none border rounded bg-gray-200 text-black border-black hover:border-transparent font-semibold hover:text-purple-800 hover:font-black hover:bg-teal-100 mt-4 lg:mt-0 ml-4'
             >
+              <Link href='#'>
               Sign Up
-            </a>
+              </Link>
+            </div>
           </div>
-        </div>
+       
       </nav>
-    </div>
+   
   );
 }
-
-export default Navbar;
