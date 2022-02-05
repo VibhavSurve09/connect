00c1a3b390b7f821bfb34407d147dfb089bf34d0
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-02-05 09:02:48 +0530

    Working with server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 const httpServer = createServer();
-const PORT = 8000;
+const PORT = 8001;
 const io = new Server(httpServer, {
   cros: {
-    origin: 'http://localhost:3000',
+    origin: '*',
   },
   /* options */
 });
+//For developement only
+io.engine.on('initial_headers', (headers, req) => {
+  headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+  headers['Access-Control-Allow-Credentials'] = true;
+});
+
+io.engine.on('headers', (headers, req) => {
+  headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+  headers['Access-Control-Allow-Credentials'] = true;
+});
+
 io.use((socket, next) => {
-  const userData = socket.handshake.auth;
-  console.log('The ddata is', userData);
-  console.log(socket.handshake);
+  const { userName, auid } = socket.handshake.auth;
+  if (!(userName && auid)) {
+    return next(new Error('Auth Required'));
+  }
+  socket.userData = { userName, auid };
+  next();
 });
 io.on('connection', (socket) => {
+  const socketActiveUsers = [];
+  for (let [id, socket] of io.of('/').sockets) {
+    socketActiveUsers.push({
+      uid: id,
+      userData: socket.userData,
+    });
+  }
+  io.emit('users', socketActiveUsers);
+  console.log(socketActiveUsers);
   // ...
 });
 
