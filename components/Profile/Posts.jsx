110451f79557045b99c5fe1fadff4353ecdb90fc
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-03-10 10:11:20 +0530

    Bug fixes

--- components/Profile/Posts.jsx
+++ components/Profile/Posts.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
-import { UserContext } from "../../context/User";
-import { useUser } from "../../hooks/useUser";
-import { useState } from "react";
-import { useEffect } from "react";
-import { getPosts } from "../../services/firebase";
-import ListOfPosts from "./ListOfPosts";
+import React, { useContext } from 'react';
+import { UserContext } from '../../context/User';
+import { useUser } from '../../hooks/useUser';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getPosts } from '../../services/firebase';
+import ListOfPosts from './ListOfPosts';
 function Posts() {
   const activeUser = useContext(UserContext);
   const { data, loading } = useUser(activeUser?.uid);
@@ -12,18 +12,20 @@ function Posts() {
   useEffect(() => {
     if (data && !loading) {
       let { postsRef } = data;
-      postsRef.map((ref) => {
-        console.log(ref);
-        getPosts(ref).then((postData) => {
-          setAllPosts((oldPosts) => [...oldPosts, postData]);
+      if (postsRef) {
+        postsRef.map((ref) => {
+          console.log(ref);
+          getPosts(ref).then((postData) => {
+            setAllPosts((oldPosts) => [...oldPosts, postData]);
+          });
         });
-      });
+      }
     }
   }, [data, activeUser]);
   return (
     <div>
       {posts.length > 0 ? (
-        <div className="w-full">
+        <div className='w-full'>
           <ListOfPosts posts={posts} />
         </div>
       ) : (
