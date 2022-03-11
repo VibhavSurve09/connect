46a4b0d72f539ba8507ae004fd2b2f30108fed0d
pandeyutkarsh17 <utkarshpandey138@gmail.com>
2022-03-11 17:16:07 +0530

    backup2

---
+++ pages/P/[postId].jsx
@@ -0,0 +1,12 @@
+import Postfocus from "../../components/Posts/Postfocus";
+import React from "react";
+
+function Post() {
+  return (
+    <div>
+      <Postfocus></Postfocus>
+    </div>
+  );
+}
+
+export default Post;
