a43b13116b92c3bf9bf81723b056b8e367ec6756
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-04-11 16:13:39 +0530

    Delete Profile

--- pages/deleteprofile.jsx
+++ pages/deleteprofile.jsx
@@ -4,13 +4,21 @@ import { UserContext } from '../context/User';
 import { deleteProfileFB, getUserDataById } from '../services/firebase';
 import { deleteProfile } from '../services/neo4j';
 import { useUser } from '../hooks/useUser';
+import { auth } from '../constants/firebase';
+import { signOut } from 'firebase/auth';
 export default function DeleteProfile({ closeButton }) {
   const activeUser = useContext(UserContext);
   const { data, loading } = useUser(activeUser?.uid);
   const deleteProfilee = async () => {
     await deleteProfile(activeUser?.uid);
     await deleteProfileFB(data.docId);
-    console.log('Deleted');
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
   };
   return (
     <div className='fixed bottom-0 left-0 flex items-center justify-center w-full h-full bg-indigo-400'>
