8bd474b3aaf8f9cf3d84f5e7995eec93ce1315d1
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-06-17 16:00:28 +0530

    Removed env from next.config.js

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ module.exports = {
       'firebasestorage.googleapis.com',
     ],
   },
-  env: {
-    JWT_SECRET: process.env.JWT_SECRET,
-    SOCKET_URI: process.env.SOCKET_URI,
-    API_URI: process.env.API_URI,
-    MONGO_URI: process.env.MONGO_URI,
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
